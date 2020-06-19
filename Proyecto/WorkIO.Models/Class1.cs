2f906727d7617b913a36e28ff289261386426f85
Carlos Lopez <60913078+cardanxd@users.noreply.github.com>
2020-06-18 19:29:07 -0500

    Actualizacion

--- Proyecto/WorkIO.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WorkIO.Models
-{
-    public class Class1
-    {
-    }
-}
--- Proyecto/WorkIO.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WorkIO.Services
-{
-    public class Class1
-    {
-    }
-}
