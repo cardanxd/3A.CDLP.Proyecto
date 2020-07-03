9c889e8b835268474bdb041e4e7bdd3a0ed4fdcf
Carlos Lopez <60913078+cardanxd@users.noreply.github.com>
2020-07-03 00:19:58 -0500

    Lista de empresas registradas se ven en Menú

---
+++ Proyecto/Work-IO/Pages/WorkIO/EmpresasRegistradas.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkIO.Models;
+using WorkIO.Services;
+
+namespace Work_IO.Pages.WorkIO
+{
+    public class EmpresasRegistradasModel : PageModel
+    {
+        public IEnumerable<Empresa> Empresas { get; set; }
+        public IWebHostEnvironment HostEnvironment { get; }
+
+        private readonly IRepository<Empresa> repository;
+
+        public EmpresasRegistradasModel(IRepository<Empresa> repository, IWebHostEnvironment hostEnvironment)
+        {
+            this.repository = repository;
+            HostEnvironment = hostEnvironment;
+        }
+        public void OnGet()
+        {
+            Empresas = repository.GetAll();
+        }
+    }
+}
