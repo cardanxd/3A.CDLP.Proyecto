dda421b14d231915b8a862e35a32acf457af84eb
Carlos Lopez <60913078+cardanxd@users.noreply.github.com>
2020-07-21 02:03:48 -0500

    Details

---
+++ Proyecto/Work-IO/Pages/WorkIO/DetailsCandidato.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkIO.Models;
+using WorkIO.Services;
+
+namespace Work_IO.Pages.WorkIO
+{
+    public class DetailsCandidatoModel : PageModel
+    {
+        private readonly IRepository<Candidato> repository;
+        public Candidato Candidato { get; private set; }
+
+        public DetailsCandidatoModel(IRepository<Candidato> repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult OnGet(int id)
+        {
+            Candidato = repository.Get(id);
+            if (Candidato.Id == 0)
+                return RedirectToPage("NotFound");
+            return Page();
+        }
+    }
+}
---
+++ Proyecto/Work-IO/Pages/WorkIO/DetailsEmpresa.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkIO.Models;
+using WorkIO.Services;
+
+namespace Work_IO.Pages.WorkIO
+{
+    public class DetailsEmpresaModel : PageModel
+    {
+        private readonly IRepository<Empresa> repository;
+        public Empresa Empresa { get; private set; }
+
+        public DetailsEmpresaModel(IRepository<Empresa> repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult OnGet(int id)
+        {
+            Empresa = repository.Get(id);
+            if (Empresa.Id == 0)
+                return RedirectToPage("NotFound");
+            return Page();
+        }
+    }
+}
