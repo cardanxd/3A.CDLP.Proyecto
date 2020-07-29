4a8d300655ac1d17eae68ebd8eb3c26da47b7c56
Carlos Lopez <60913078+cardanxd@users.noreply.github.com>
2020-07-29 12:00:10 -0500

    Proyecto final

---
+++ Proyecto/Work-IO/Pages/WorkIO/DeleteReferencia.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WorkIO.Models;
+using WorkIO.Services;
+
+namespace Work_IO.Pages.WorkIO
+{
+    public class DeleteReferenciaModel : PageModel
+    {
+        [BindProperty]
+        public Referencia Referencia { get; set; }
+
+        private readonly AppDBContext _context;
+        public DeleteReferenciaModel(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            Referencia = await _context.Referencias.FindAsync(id);
+
+            if (Referencia == null)
+            {
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Referencia = await _context.Referencias.FindAsync(id);
+
+            if (Referencia != null)
+            {
+                _context.Referencias.Remove(Referencia);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
